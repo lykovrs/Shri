3d1f3658e081d57eb265271bfd5c9c87f0bc1fff
lykovrs <lykovrs@gmail.com>
2018-01-10 16:54:46 +0300

    add first component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Designator from './components/Designator';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
+      <div>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Designator text={'Ржавый Фред'} helperText={'3—6 человек'} />
       </div>
     );
   }
