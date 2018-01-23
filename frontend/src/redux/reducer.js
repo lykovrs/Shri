542bd3ed36a19c94138fa51d181fbd275bdc65f4
lykovrs <lykovrs@gmail.com>
2018-01-23 17:36:57 +0300

    add evens duck

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 import roomsReducer, { moduleName as roomsModule } from '../ducks/rooms';
-// import peopleReducer, {moduleName as peopleModule} from '../ducks/people'
-// import eventsReducer, {moduleName as eventsModule} from '../ducks/events'
+import eventsReducer, { moduleName as eventsModule } from '../ducks/events';
 
 export default combineReducers({
   router,
   form,
-  [roomsModule]: roomsReducer
-  // [peopleModule]: peopleReducer,
-  // [eventsModule]: eventsReducer,
+  [roomsModule]: roomsReducer,
+  [eventsModule]: eventsReducer
 });
--- frontend/src/redux/saga.js
+++ frontend/src/redux/saga.js
@@ -1,12 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { saga as roomsSaga } from '../ducks/rooms';
-// import {saga as authSaga} from '../ducks/auth'
-// import {saga as eventsSaga} from '../ducks/events'
+import { saga as eventsSaga } from '../ducks/events';
 
 export default function* rootSaga() {
-  yield all([
-    roomsSaga()
-    // authSaga(),
-    // eventsSaga()
-  ]);
+  yield all([roomsSaga(), eventsSaga()]);
 }
